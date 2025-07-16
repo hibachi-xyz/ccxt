e4292598dcb951ff9397bf23df1c92912336da92
vincent-hibachi-xyz <vincent@hibachi.xyz>
2025-07-16 13:00:16 -0400

    abstract

--- ts/src/abstract/hibachi.ts
+++ ts/src/abstract/hibachi.ts
@@ -16,6 +16,7 @@ interface Exchange {
     publicGetMarketDataOrderbook (params?: {}): Promise<implicitReturnType>;
     privateGetTradeAccountInfo (params?: {}): Promise<implicitReturnType>;
     privateGetTradeAccountTrades (params?: {}): Promise<implicitReturnType>;
+    privateGetTradeOrders (params?: {}): Promise<implicitReturnType>;
     privatePutTradeOrder (params?: {}): Promise<implicitReturnType>;
     privateDeleteTradeOrder (params?: {}): Promise<implicitReturnType>;
     privatePostTradeOrder (params?: {}): Promise<implicitReturnType>;
